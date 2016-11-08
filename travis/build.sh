fce661416300add158fbea69d3dc5cd0595cf648
Martin Wicks <wicksy@wicksy.com>
2016-11-08 09:25:16 +0000

    Run States and Playbook in build

--- travis/build.sh
+++ travis/build.sh
@@ -3,9 +3,26 @@
 # Build - Apply Salt States and Ansible Playbooks
 #
 
-# Check versions installed
+# Exit on errors
 #
-salt-call --version
-ansible-playbook --version
+#set -e
+#set -u
+#set -o pipefail
+
+# Create Salt Links for States and Pillar
+# Create Ansible Links for Playbooks and Variables
+#
+mkdir -p /srv
+ln -sf "$(pwd)/salt/roots" /srv/salt
+ln -sf "$(pwd)/salt/pillar" /srv/pillar
+ln -sf "$(pwd)/ansible" /srv/ansible
+
+# Apply Salt States
+#
+salt-call --local -l debug state.apply
+
+# Apply Ansible Plays
+#
+cd /srv/ansible && /usr/bin/ansible-playbook -i hosts site.yml -vv --limit=travis-ci
 
 exit 0
