b3086371b5ead4fe6f8d3423fb45d94065b7f5cf
Martin Wicks <wicksy@wicksy.com>
2017-04-09 18:01:22 +0100

    Add tests for new links

--- test/test_links.py
+++ test/test_links.py
@@ -4,6 +4,10 @@ import pytest
   ("/home/wicksy/vagrant/Vagrantfile", "/git/wicksy/configfiles/vagrant/Vagrantfile"),
   ("/home/wicksy/.vimrc", "/git/wicksy/configfiles/dotfiles/.vimrc"),
   ("/home/wicksy/.bashrc", "/git/wicksy/configfiles/dotfiles/.bashrc"),
+  ("/home/wicksy/.aws", "/git/wicksy/configfiles/dotfiles/.aws"),
+  ("/home/wicksy/.config", "/git/wicksy/configfiles/dotfiles/.config"),
+  ("/home/wicksy/.gitconfig", "/git/wicksy/configfiles/dotfiles/.gitconfig"),
+  ("/home/wicksy/.ssh/config", "/git/wicksy/configfiles/dotfiles/.ssh/config"),
 ])
 
 def test_links(File, name, linked_to):
